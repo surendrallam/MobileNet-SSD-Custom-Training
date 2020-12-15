57e358fff8b69f5d4a552abcaab5aeb65f8ae410
Surendra <surendra.a@exaleapsemi.com>
2020-12-15 11:30:25 +0530

    Creating MobileNet-SSD-Custom-Training repo with the data

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if ! test -f example/MobileNetSSD_train.prototxt ;then
+	echo "error: example/MobileNetSSD_train.prototxt does not exist."
+	echo "please use the gen_model.sh to generate your own model."
+        exit 1
+fi
+mkdir -p snapshot
+/home/surendra/surendra/ssd/caffe_wille/build/tools/caffe train -solver="solver_train.prototxt" \
+-weights="examples/mobilenet_iter_73000.caffemodel" \
+#-snapshot="snapshot/mobilenet_iter_10.solverstate"
+#-cpu 0 
